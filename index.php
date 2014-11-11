c76df3349afbeb34882acb020812e30c484e2180
maardz <moldermarge@gmail.com>
2014-11-12 00:08:35 +0200

    Innital commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
++<html>
++<head>
++  <title></title>
++  <meta charset="UTF-8">
++
++</head>
++<body>
++  <h1>1. page</h1>
++</body>
++</html>
---
+++ second_page.php
@@ -0,0 +1,12 @@
++<!DOCTYPE HTML>
++<html>
++<head>
++  <title></title>
++  <meta charset="UTF-8">
++  <link rel="stylesheet" href="style.css" type="text/css">
++
++</head>
++<body>
++	<h1>2. page</h1>
++</body>
++</html>
