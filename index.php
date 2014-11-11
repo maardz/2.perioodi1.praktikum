b78b66f405be4e266a3950032f5528275761460f
maardz <moldermarge@gmail.com>
2014-11-12 01:04:23 +0200

    Add url parameters

--- index.php
+++ index.php
@@ -1,11 +1,26 @@
 <!DOCTYPE HTML>
-+<html>
-+<head>
-+  <title></title>
-+  <meta charset="UTF-8">
-+
-+</head>
-+<body>
-+  <h1>1. page</h1>
-+</body>
-+</html>
+<html>
+<head>
+  <title></title>
+  <meta charset="UTF-8">
+
+</head>
+<body>
+  <h1>1. page</h1>
+
+	<?php
+     $link_file = "second_page.php";
+     $name = "Mölder";
+    ?>
+   
+   <a href="<?php echo "{$link_file}?name={$name}"; ?>">Teisele lehele</a><br>
+    <?php 
+      print_r($_GET); echo "<br>";
+      echo $_GET["firstname"];
+      $jada = array("Marge", "Mölder"); echo "<br>";
+      echo $jada[1];
+    ?>
+	</a>
+
+</body>
+</html>
--- second_page.php
+++ second_page.php
@@ -1,12 +1,16 @@
-+<!DOCTYPE HTML>
-+<html>
-+<head>
-+  <title></title>
-+  <meta charset="UTF-8">
-+  <link rel="stylesheet" href="style.css" type="text/css">
-+
-+</head>
-+<body>
-+	<h1>2. page</h1>
-+</body>
-+</html>
+<!DOCTYPE HTML>
+<html>
+<head>
+  <title></title>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="style.css" type="text/css">
+
+</head>
+<body>
+	<h1>2. page</h1>
+	<a href="index.php?firstname=Marge&name=Mölder">First page</a><br>
+	 <?php 
+      print_r($_GET);
+    ?>
+</body>
+</html>
